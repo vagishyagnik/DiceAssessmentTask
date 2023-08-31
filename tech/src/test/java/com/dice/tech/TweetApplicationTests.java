f17b4e82b7a18f1366f7bfe26b38d319ec21b7f7
vagishYagnik87 <vagishyagnik@gmail.com>
2023-08-31 15:18:51 +0530

    Initial Application Setup

---
+++ tech/src/test/java/com/dice/tech/TweetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dice.tech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TweetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
