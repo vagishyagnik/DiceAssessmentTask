c4dd4f2cf2de8dc6bb6b1d3063eb6bcc7ed74b02
vagishYagnik87 <vagishyagnik@gmail.com>
2023-09-01 18:06:42 +0530

    Completed controllers service & app

--- tech/src/main/java/com/dice/tech/WeatherApplication.java
+++ tech/src/main/java/com/dice/tech/WeatherApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TweetApplication {
+public class WeatherApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TweetApplication.class, args);
+		SpringApplication.run(WeatherApplication.class, args);
 	}
 
 }
--- tech/src/test/java/com/dice/tech/WeatherApplicationTests.java
+++ tech/src/test/java/com/dice/tech/WeatherApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class TweetApplicationTests {
+class WeatherApplicationTests {
 
 	@Test
 	void contextLoads() {
