a82c883f9d9210eba2ea3fc426529e88ee52048f
vagishYagnik87 <vagishyagnik@gmail.com>
2023-09-01 19:05:58 +0530

    Added updates required for cors

--- tech/src/main/java/com/dice/tech/WeatherApplication.java
+++ tech/src/main/java/com/dice/tech/WeatherApplication.java
@@ -1,13 +1,30 @@
 package com.dice.tech;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WeatherApplication {
 
+	@Value("${client.app}")
+	private String clientAppUrl;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins(clientAppUrl);
+			}
+		};
 	}
 
 }
