b0eb6e13b4e219256c34855604bea056335c9bf4
vagishYagnik87 <vagishyagnik@gmail.com>
2023-09-04 01:25:56 +0530

    Updated Controllers & interceptors & added spring security to them

--- tech/src/main/java/com/dice/tech/WeatherApplication.java
+++ tech/src/main/java/com/dice/tech/WeatherApplication.java
@@ -1,17 +1,10 @@
 package com.dice.tech;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class WeatherApplication {
-
-	@Value("${client.app}")
-	private String clientAppUrl;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WeatherApplication.class, args);
