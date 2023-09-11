5ce090e774bc1972a0cdd8e4a680174c191ea148
vagishYagnik87 <vagishyagnik@gmail.com>
2023-09-11 17:00:30 +0530

    Added Authentication via JWT Token

--- tech/src/main/java/com/dice/tech/controllers/AuthController.java
+++ tech/src/main/java/com/dice/tech/controllers/AuthController.java
@@ -1,11 +1,14 @@
 package com.dice.tech.controllers;
 
+import com.dice.tech.jwt.JwtHelper;
+import com.dice.tech.jwt.JwtResponse;
 import com.dice.tech.model.Client;
 import com.dice.tech.model.ClientBody;
 import com.dice.tech.service.ClientService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +24,9 @@ public class AuthController {
     @Autowired
     private ClientService clientService;
 
+    @Autowired
+    private JwtHelper helper;
+
     @PostMapping("/signup")
     public ResponseEntity<Client> signup(@RequestBody ClientBody clientBody) {
         try {
@@ -32,11 +38,15 @@ public class AuthController {
     }
 
     @PostMapping("/login")
-    public ResponseEntity<Client> login(@RequestBody ClientBody clientBody) {
+    public ResponseEntity<JwtResponse> login(@RequestBody ClientBody clientBody) {
         try {
             Boolean isAVerifiedClient = this.clientService.verifyClient(clientBody.getClientName(), clientBody.getClientSecret());
             if (isAVerifiedClient) {
-                return ResponseEntity.ok(this.clientService.getClientByClientName(clientBody.getClientName()));
+                Client client = this.clientService.getClientByClientName(clientBody.getClientName());
+                String token = this.helper.generateToken(client);
+
+                JwtResponse response = new JwtResponse(token, client.getClientName());
+                return new ResponseEntity<>(response, HttpStatus.OK);
             } else {
                 throw new Exception("Not a Valid Client" +  clientBody.toString());
             }
@@ -56,4 +66,8 @@ public class AuthController {
         }
     }
 
+// 1. I have to expose one more api of signin or login
+// have to implement the JWT mechanism
+// will throw a jwt access token
+// 2. Other weather controller apis will be authenticated by that JWT
 }
---
+++ tech/src/main/java/com/dice/tech/jwt/JwtHelper.java
@@ -0,0 +1,62 @@
+package com.dice.tech.jwt;
+
+import com.dice.tech.model.Client;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+
+@Component
+public class JwtHelper {
+    public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+
+    @Value("${jwt.secret}")
+    private String secret;
+
+    public JwtHelper() {}
+
+    public String getClientFromToken(String token) {
+        return getClaimFromToken(token, Claims::getSubject);
+    }
+
+    public Date getExpirationDateFromToken(String token) {
+        return getClaimFromToken(token, Claims::getExpiration);
+    }
+
+    public <T> T getClaimFromToken(String token, Function<Claims, T> claimsResolver) {
+        final Claims claims = getAllClaimsFromToken(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Claims getAllClaimsFromToken(String token) {
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private Boolean isTokenExpired(String token) {
+        final Date expiration = getExpirationDateFromToken(token);
+        return expiration.before(new Date());
+    }
+
+    //generate token for user
+    public String generateToken(Client client) {
+        Map<String, Object> claims = new HashMap<>();
+        return doGenerateToken(claims, client.getClientName());
+    }
+
+    private String doGenerateToken(Map<String, Object> claims, String subject) {
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
+    public Boolean validateToken(String token, Client client) {
+        final String username = getClientFromToken(token);
+        return (username.equals(client.getClientName()) && !isTokenExpired(token));
+    }
+}
